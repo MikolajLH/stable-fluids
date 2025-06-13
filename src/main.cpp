6996a9f0908c9da636b5ef1e1653dbc1a424b8a8
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-13 23:50:31 +0200

    basic main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
+#include <unordered_map>
+#include <algorithm>
+#include <random>
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "graphics/BaseWindow.h"
+
+class Demo : public wnd::BaseWindow {
+
+public:
+
+	Demo(size_t rs, size_t cs)
+		:
+		BaseWindow(800, 600, "Demo!") {
+
+	}
+
+	void update(float dt) override {
+
+		wnd::clear_screen(0.5f, 0.3f, 0.6f);
+	}
+};
 
 
 int main() {
+	Demo window(200, 200);
+	window.run();
+
 	return 0;
 }
