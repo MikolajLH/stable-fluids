b5ca7de554942bc625d4a0b3c888094fecf28af1
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-13 23:49:18 +0200

    Cmake config for OpenGL

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,23 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    glfw
+    GIT_REPOSITORY https://github.com/glfw/glfw.git
+    GIT_TAG latest
+)
+FetchContent_MakeAvailable(glfw)
+
+
+set(glew-cmake_BUILD_SHARED NO)
+set(ONLY_LIBS ON)
+FetchContent_Declare(
+  glew
+  GIT_REPOSITORY https://github.com/Perlmint/glew-cmake.git
+  GIT_TAG        origin/master
+)
+FetchContent_GetProperties(glew)
+if(NOT glew_POPULATED)
+  FetchContent_Populate(glew)
+
+  add_subdirectory(${glew_SOURCE_DIR} ${glew_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
