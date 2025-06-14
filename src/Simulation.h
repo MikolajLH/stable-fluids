6049fe810f981623551a17314570da8c2643909a
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-14 18:55:53 +0200

    outline

--- src/Simulation.h
+++ src/Simulation.h
@@ -7,7 +7,16 @@ public:
 
 	void update(float dt);
 
+	void add_source();
+
 private:
 	size_t N;
 	std::vector<float>data;
+
+	void diffuse();
+	void advect();
+	void project();
+	void set_bnd();
+	void vel_step();
+	void den_step();
 };
