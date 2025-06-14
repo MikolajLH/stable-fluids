2674e727198b89d9a2603db656d0b8bbbff7047a
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-14 19:00:08 +0200

    empty functions bodies

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,3 +1,34 @@
 #include "Simulation.h"
 
 
+void Simulation::add_source() {
+
+}
+
+void Simulation::advect() {
+
+}
+
+void Simulation::den_step() {
+
+}
+
+void Simulation::vel_step() {
+
+}
+
+void Simulation::diffuse() {
+
+}
+
+void Simulation::project() {
+
+}
+
+void Simulation::set_bnd() {
+
+}
+
+void Simulation::update(float dt) {
+
+}
