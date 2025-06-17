e22279027ee887a73b775722eadafe5ed89e8f2f
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-18 01:00:49 +0200

    Sim.h

---
+++ src/Sim.h
@@ -0,0 +1,60 @@
+#pragma once
+
+class Simulation
+{
+public:
+	static constexpr int Nx = 32;
+	static constexpr int Ny = 64;
+	static constexpr int Rs = Ny + 2;
+	static constexpr int Cs = Nx + 2;
+	static constexpr int SIZE = Rs * Cs;
+	static constexpr int DIFFUSE_ITER = 30;
+	static constexpr int PROJECT_ITER = 30;
+
+	static constexpr float minX = 1.f;
+	static constexpr float maxX = 32.f;
+	static constexpr float minY = 1.f;
+	static constexpr float maxY = 64.f;
+
+	static constexpr int IX(int x, int y) { return Cs * y + x; }
+
+
+	Simulation();
+	~Simulation();
+
+	void reset();
+	void clear_buffer();
+	void set_bnd(float* arr, int flag);
+	void project();
+	void advect(float* value, float* value0, float* u, float* v, int flag);
+	void diffuse(float* value, float* value0, float rate, int flag);
+	void vort_confinement();
+	void add_source();
+	void vel_step();
+	void den_step();
+
+
+	float* vx;
+	float* vy;
+	float* vx0;
+	float* vy0;
+	float* d;
+	float* d0;
+	float* px;
+	float* py;
+	float* div;
+	float* p;
+	//vorticity confinement
+	float* vort;
+	float* absVort;
+	float* gradVortX;
+	float* gradVortY;
+	float* lenGrad;
+	float* vcfx;
+	float* vcfy;
+
+	float visc;
+	float diff;
+	float vorticity;
+	float dt;
+};
